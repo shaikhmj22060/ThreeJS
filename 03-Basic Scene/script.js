3ad39e028b83f15c898ae6c6d17c09e7ea1413e6
shaikhmj22060 <shaikhm.ja.z@gmail.com>
2026-05-10 07:59:35 +0530

    moved the camera out of the object by setting camera Z index

--- 03-Basic Scene/script.js
+++ 03-Basic Scene/script.js
@@ -4,7 +4,7 @@ const scene = new THREE.Scene();
 // Red Cube 
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const material = new THREE.MeshBasicMaterial({ color: 'red' });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
@@ -16,12 +16,15 @@ const size = {
 
 //camera
 const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
+camera.position.z = 3;
 scene.add(camera)
 
 // Canvas
 const canvas = document.querySelector(".webgl");
 // Renderer
 
-const rendere = new THREE.WebGLRenderer({
+const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
+
+renderer.setSize(size.width, size.height)
